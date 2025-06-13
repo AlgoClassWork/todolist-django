c60a47c6d348e8ca6cd0e0627c55965b701eaa15
AlgoClassWork <kyxec.play@gmail.com>
2025-06-13 12:03:43 +0600

    Add files via upload

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    title = models.CharField('Название задачи', max_length=200 )
+    completed = models.BooleanField('Завершено', default=False )
+    created_at = models.DateTimeField( auto_now_add=True )
+
+    def __str__(self):
+        return self.title
+
